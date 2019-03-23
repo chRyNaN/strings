a5fb4b4cb1c9cc2cc56c268fbde791a1f80b63ee
Chris <ckeenan@starry.com>
2019-03-23 12:04:15 -0400

    Created IntegerCreatorUtils

---
+++ api/src/commonMain/kotlin/com.chrynan.resources/IntegerCreatorUtils.kt
@@ -0,0 +1,87 @@
+package com.chrynan.resources
+
+data class IntegerResourcesFile(
+    val identifier: ResourceFileIdentifier,
+    val integerResources: Set<IntegerResource>,
+    val integerArrayResources: Set<IntegerArrayResource>
+)
+
+data class IntegerResource(
+    val identifier: ResourceIdentifier,
+    val value: Int
+)
+
+data class IntegerArrayResource(
+    val identifier: ResourceIdentifier,
+    val values: List<Int>
+)
+
+class IntegerResourcesFileBuilder(private val identifier: ResourceFileIdentifier) {
+
+    private val integerResources = mutableSetOf<IntegerResource>()
+    private val integerArrayResources = mutableSetOf<IntegerArrayResource>()
+
+    fun integer(identifier: ResourceIdentifier, value: Int) {
+        integerResources += IntegerResource(identifier = identifier, value = value)
+    }
+
+    fun integer(identifier: ResourceIdentifier, accessor: () -> Int) {
+        integerResources += IntegerResource(identifier = identifier, value = accessor.invoke())
+    }
+
+    fun integer(name: String, value: Int) {
+        integerResources += IntegerResource(identifier = NameResourceIdentifier(name = name), value = value)
+    }
+
+    fun integer(name: String, accessor: () -> Int) {
+        integerResources += IntegerResource(identifier = NameResourceIdentifier(name = name), value = accessor.invoke())
+    }
+
+    fun array(identifier: ResourceIdentifier, builder: IntegerArrayBuilder.() -> Unit) {
+        val arrayBuilder = IntegerArrayBuilder(identifier = identifier)
+        builder.invoke(arrayBuilder)
+        integerArrayResources += arrayBuilder.build()
+    }
+
+    fun array(name: String, builder: IntegerArrayBuilder.() -> Unit) {
+        val arrayBuilder = IntegerArrayBuilder(identifier = NameResourceIdentifier(name = name))
+        builder.invoke(arrayBuilder)
+        integerArrayResources += arrayBuilder.build()
+    }
+
+    internal fun build() = IntegerResourcesFile(
+        identifier = identifier,
+        integerResources = integerResources,
+        integerArrayResources = integerArrayResources
+    )
+}
+
+class IntegerArrayBuilder(private val identifier: ResourceIdentifier) {
+
+    private val arrayValues = mutableListOf<Int>()
+
+    fun item(value: Int) {
+        arrayValues += value
+    }
+
+    fun item(accessor: () -> Int) {
+        arrayValues += accessor.invoke()
+    }
+
+    internal fun build() = IntegerArrayResource(identifier = identifier, values = arrayValues)
+}
+
+fun integers(
+    identifier: ResourceFileIdentifier,
+    builder: IntegerResourcesFileBuilder.() -> Unit
+): IntegerResourcesFile {
+    val fileBuilder = IntegerResourcesFileBuilder(identifier = identifier)
+    builder.invoke(fileBuilder)
+    return fileBuilder.build()
+}
+
+fun integers(name: String, builder: IntegerResourcesFileBuilder.() -> Unit): IntegerResourcesFile {
+    val fileBuilder = IntegerResourcesFileBuilder(identifier = NameResourceFileIdentifier(name = name))
+    builder.invoke(fileBuilder)
+    return fileBuilder.build()
+}
