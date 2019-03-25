ab8b8b4a173a0182726d84b4e1ad45a60874553d
Chris <ckeenan@starry.com>
2019-03-25 17:19:47 -0400

    Created ResourcesPlugin

---
+++ plugin/src/main/kotlin/com/chrynan/resources/ResourcesPlugin.kt
@@ -0,0 +1,11 @@
+package com.chrynan.resources
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ResourcesPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+    }
+}
