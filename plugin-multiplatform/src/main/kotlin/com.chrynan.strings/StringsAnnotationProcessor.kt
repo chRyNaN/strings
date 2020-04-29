c11dbbbb43178a4336eb97869c5c4d35c2fc54a9
Chris <ckeenan@starry.com>
2020-04-29 15:43:54 -0400

    Created StringsAnnotationProcessor

---
+++ plugin-multiplatform/src/main/kotlin/com.chrynan.strings/StringsAnnotationProcessor.kt
@@ -0,0 +1,8 @@
+package com.chrynan.strings
+
+class StringsAnnotationProcessor : BaseStringsAnnotationProcessor() {
+
+    override fun handleAnnotatedFiles(annotatedFileElements: Set<AnnotatedFileElement>) {
+
+    }
+}
