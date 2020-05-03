71df098e14b494c22e11f423703b0594b8ff6926
Chris <ckeenan@starry.com>
2020-05-02 23:39:27 -0400

    Updated Strings object to implement StringReviser interface

--- accessor/src/commonMain/kotlin/com/chrynan/strings/accessor/Strings.kt
+++ accessor/src/commonMain/kotlin/com/chrynan/strings/accessor/Strings.kt
@@ -1,11 +1,13 @@
 package com.chrynan.strings.accessor
 
 import com.chrynan.strings.*
-import kotlin.String
 
-object Strings : StringAccessor {
+object Strings : StringAccessor,
+    StringReviser {
 
     lateinit var accessor: StringAccessor
+
+    lateinit var reviser: StringReviser
 
     override fun getStaticString(resourceID: StaticStringResourceID, locale: String): String =
         accessor.getStaticString(resourceID = resourceID, locale = locale)
@@ -24,6 +26,30 @@ object Strings : StringAccessor {
         locale: String,
         quantity: Quantity,
         vararg arguments: Any
-    ): String =
-        accessor.getPluralString(resourceID = resourceID, locale = locale, quantity = quantity, arguments = *arguments)
+    ): String = accessor.getPluralString(resourceID = resourceID, locale = locale, quantity = quantity)
+
+    override val updateListeners: MutableSet<StringUpdateListener>
+        get() = reviser.updateListeners
+
+    override fun updateStaticString(resourceID: StaticStringResourceID, locale: String, value: String) =
+        reviser.updateStaticString(resourceID = resourceID, locale = locale, value = value)
+
+    override fun updateDynamicString(resourceID: DynamicStringResourceID, locale: String, value: String) =
+        reviser.updateDynamicString(resourceID = resourceID, locale = locale, value = value)
+
+    override fun updateHtmlString(resourceID: HtmlStringResourceID, locale: String, value: String) =
+        reviser.updateHtmlString(resourceID = resourceID, locale = locale, value = value)
+
+    override fun updateStringArray(resourceID: StringArrayResourceID, locale: String, value: Array<String>) =
+        reviser.updateStringArray(resourceID = resourceID, locale = locale, value = value)
+
+    override fun updatePluralStrings(
+        resourceID: PluralStringResourceID,
+        locale: String,
+        values: Map<Quantity, String>
+    ) = reviser.updatePluralStrings(
+        resourceID = resourceID,
+        locale = locale,
+        values = values
+    )
 }
