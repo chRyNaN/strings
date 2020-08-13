9ce174fd8f1dd04b81bad62876214b50aa4cd53f
Chris <ckeenan@starry.com>
2020-08-13 11:55:45 -0400

    Created LibraryConstants in buildSrc

---
+++ buildSrc/src/main/java/com.chrynan.strings.buildSrc/LibraryConstants.kt
@@ -0,0 +1,14 @@
+package com.chrynan.strings.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.strings"
+    const val owner = "chrynan"
+    const val repoName = "strings"
+    const val versionName = "0.1.0"
+    const val versionCode = 1
+    const val versionDescription = "First Major Release"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/strings.git"
+}
