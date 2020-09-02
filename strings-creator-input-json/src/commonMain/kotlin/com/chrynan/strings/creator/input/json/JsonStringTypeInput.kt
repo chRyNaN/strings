44379d95b8f1da26c208ab6224aa19ea45baf3f6
Chris <ckeenan@starry.com>
2020-09-02 13:13:01 -0400

    Updated JsonStringTypeParser to handle optional locales

--- strings-creator-input-json/src/commonMain/kotlin/com/chrynan/strings/creator/input/json/JsonStringTypeInput.kt
+++ strings-creator-input-json/src/commonMain/kotlin/com/chrynan/strings/creator/input/json/JsonStringTypeInput.kt
@@ -2,7 +2,17 @@ package com.chrynan.strings.creator.input.json
 
 import com.chrynan.strings.core.Locale
 
+/**
+ * Represents a JSON String Type File as input for the [JsonStringTypeParser].
+ *
+ * @property [fileName] The name of the file being parsed.
+ * @property [fileLocale] The optional locale provided in the file name. Each element can override
+ * this value with their own [JsonStringValue.locale] property. But if one isn't present, then this
+ * value is used, and if this value isn't present, then the [Locale.default] value is used.
+ * @property [jsonString] The [String] representation of the JSON from the file.
+ */
 data class JsonStringTypeInput(
-    val fileLocale: String = Locale.default,
+    val fileName: String,
+    val fileLocale: String? = null,
     val jsonString: String
 )
--- strings-creator-input-json/src/commonMain/kotlin/com/chrynan/strings/creator/input/json/JsonStringTypeParser.kt
+++ strings-creator-input-json/src/commonMain/kotlin/com/chrynan/strings/creator/input/json/JsonStringTypeParser.kt
@@ -1,5 +1,6 @@
 package com.chrynan.strings.creator.input.json
 
+import com.chrynan.strings.core.Locale
 import com.chrynan.strings.core.Quantity
 import com.chrynan.strings.creator.core.StringType
 import com.chrynan.strings.creator.core.StringTypeName
@@ -8,6 +9,13 @@ import kotlinx.serialization.builtins.list
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 
+/**
+ * Converts a [JsonStringTypeInput] into a [List] of [StringType]s so that they can be used to
+ * generate necessary string files. The [JsonStringTypeInput] represents a single JSON file
+ * following the String Type JSON Format. To parse multiple files, create multiple
+ * [JsonStringTypeInput] instances and call the [parse] function with each of them as input, and
+ * aggregate their results.
+ */
 class JsonStringTypeParser : StringTypeParser<JsonStringTypeInput> {
 
     override fun parse(input: JsonStringTypeInput): List<StringType> {
@@ -25,27 +33,43 @@ class JsonStringTypeParser : StringTypeParser<JsonStringTypeInput> {
             }
     }
 
-    private fun mapStringValue(value: JsonStringValue.StringValue, locale: String): StringType =
+    private fun mapStringValue(value: JsonStringValue.StringValue, fileLocale: String?): StringType =
         when (StringTypeName.fromName(value.type)) {
-            StringTypeName.STATIC -> StringType.Static(name = value.name, value = value.value, locale = locale)
-            StringTypeName.DYNAMIC -> StringType.Dynamic(name = value.name, value = value.value, locale = locale)
-            StringTypeName.HTML -> StringType.Html(name = value.name, value = value.value, locale = locale)
+            StringTypeName.STATIC -> StringType.Static(
+                name = value.name,
+                value = value.value,
+                locale = value.locale ?: fileLocale ?: Locale.default
+            )
+            StringTypeName.DYNAMIC -> StringType.Dynamic(
+                name = value.name,
+                value = value.value,
+                locale = value.locale ?: fileLocale ?: Locale.default
+            )
+            StringTypeName.HTML -> StringType.Html(
+                name = value.name,
+                value = value.value,
+                locale = value.locale ?: fileLocale ?: Locale.default
+            )
             else -> throw InvalidStringValueTypeException(
                 expectedType = StringTypeName.STATIC.value,
                 actualType = value.type
             )
         }
 
-    private fun mapArrayValue(value: JsonStringValue.ArrayValue, locale: String): StringType =
+    private fun mapArrayValue(value: JsonStringValue.ArrayValue, fileLocale: String?): StringType =
         when (StringTypeName.fromName(value.type)) {
-            StringTypeName.ARRAY -> StringType.Array(name = value.name, values = value.values, locale = locale)
+            StringTypeName.ARRAY -> StringType.Array(
+                name = value.name,
+                values = value.values,
+                locale = value.locale ?: fileLocale ?: Locale.default
+            )
             else -> throw InvalidStringValueTypeException(
                 expectedType = StringTypeName.STATIC.value,
                 actualType = value.type
             )
         }
 
-    private fun mapPluralsValue(value: JsonStringValue.PluralsValue, locale: String): StringType =
+    private fun mapPluralsValue(value: JsonStringValue.PluralsValue, fileLocale: String?): StringType =
         when (StringTypeName.fromName(value.type)) {
             StringTypeName.ARRAY -> StringType.Plurals(
                 name = value.name,
@@ -55,7 +79,7 @@ class JsonStringTypeParser : StringTypeParser<JsonStringTypeInput> {
                         quantity = Quantity.fromName(it.quantity) ?: Quantity.ONE
                     )
                 },
-                locale = locale
+                locale = value.locale ?: fileLocale ?: Locale.default
             )
             else -> throw InvalidStringValueTypeException(
                 expectedType = StringTypeName.STATIC.value,
